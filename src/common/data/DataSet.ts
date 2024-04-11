39fac7361e315ccd6e3c870c40f7131e360ca905
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-11 20:44:28 +0300

    fix(data) changing the source data

--- src/common/data/DataSet.ts
+++ src/common/data/DataSet.ts
@@ -1,60 +1,73 @@
-export type ButtonTextType = 'Cool!' | 'Awesome!' | 'Try now!';
-
-export type DataSetType = {
+export type CoolArrType = {
   id: string;
   title: string;
-  buttonText: ButtonTextType;
+  description: string;
 };
 
-export const DataSet: DataSetType[] = [
+export type AwesomeArrType = {
+  id: string;
+  title: string;
+  description: string;
+  anotherDescription: string;
+};
+
+export type TryNowArrType = {
+  id: string;
+  title: string;
+};
+
+export const CoolArr: CoolArrType[] = [
   {
     id: '1',
     title: 'You have',
-    buttonText: 'Cool!',
+    description: 'millions of questions...',
   },
   {
     id: '2',
     title: 'We have',
-    buttonText: 'Cool!',
+    description: 'answers in the stars!',
+  },
+];
+export const AwesomeArr: AwesomeArrType[] = [
+  {
+    id: '1',
+    title: 'Love',
+    description: 'When will I meet my soulmate?',
+    anotherDescription: 'When will I get married?',
+  },
+  {
+    id: '2',
+    title: 'Self',
+    description: 'What I am here to learn in life?',
+    anotherDescription: ' Am I on the right path?',
   },
   {
     id: '3',
-    title: 'Love',
-    buttonText: 'Awesome!',
+    title: 'Daily life',
+    description: 'What is going to happen next?',
+    anotherDescription: 'Any insight for today?',
+  },
+];
+
+export const TryNowArr: TryNowArrType[] = [
+  {
+    id: '1',
+    title: 'How it works',
+  },
+  {
+    id: '2',
+    title: 'Share birth details',
+  },
+  {
+    id: '3',
+    title: 'Ask a question',
   },
   {
     id: '4',
-    title: 'Self',
-    buttonText: 'Awesome!',
+    title: 'An astrologer will make a prediction',
   },
   {
     id: '5',
-    title: 'Daily life',
-    buttonText: 'Awesome!',
-  },
-  {
-    id: '6',
-    title: 'How it works',
-    buttonText: 'Try now!',
-  },
-  {
-    id: '7',
-    title: 'Share birth details',
-    buttonText: 'Try now!',
-  },
-  {
-    id: '8',
-    title: 'Ask a question',
-    buttonText: 'Try now!',
-  },
-  {
-    id: '9',
-    title: 'An astrologer will make a prediction',
-    buttonText: 'Try now!',
-  },
-  {
-    id: '10',
     title: 'Get your answer',
-    buttonText: 'Try now!',
   },
 ];
