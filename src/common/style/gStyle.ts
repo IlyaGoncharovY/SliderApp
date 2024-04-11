2ccb06de97cfcfc5811b0c9fe06b70f2f0f1e282
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-11 23:50:24 +0300

    feat(gStyle) added common style

---
+++ src/common/style/gStyle.ts
@@ -0,0 +1,10 @@
+import {StyleSheet} from 'react-native';
+
+export const gStyles = StyleSheet.create({
+  gBackColor: {
+    backgroundColor: '#FFFFFF',
+  },
+  gColor: {
+    color: '#ffffff',
+  },
+});
