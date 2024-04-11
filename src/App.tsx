49818fcf41fffad756b190cfa1eb17cfe6ed3ce8
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-04-11 16:23:42 +0300

    fix(App) added LinearGradient for layout app

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import {Slider} from './features/body';
+import {Header} from './features/header';
+import {Pagination} from './features/footer/Pagination.tsx';
 
 export const App = () => {
   return (
-    <SafeAreaView style={styles.container}>
-      <Slider />
-    </SafeAreaView>
+    <LinearGradient colors={['#6B73FF', '#000DFF']} style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <Header />
+        <Slider />
+        <Pagination />
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
